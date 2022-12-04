335b044d0f09f925ba01c97cfc711cbcca6f600e
Van Vien Dong <vanviendong96@gmail.com>
2022-12-04 16:53:28 +0700

    feature: implement common loading

---
+++ src/components/common/Loading/Loading.tsx
@@ -0,0 +1,27 @@
+import LoadingIcon from "@assets/icons/loading-icon.svg";
+import styles from "./Loading.module.scss";
+import styled from "styled-components";
+
+interface ILoading {
+  loading: boolean;
+  size?: string;
+}
+
+const Spinner = styled.div`
+  width: ${({ size }: any) => size ?? "90px"};
+  height: ${({ size }: any) => size ?? "90px"};
+` as any;
+
+const Loading: React.FC<ILoading> = ({ loading, size }) => {
+  return (
+    <>
+      {loading && (
+        <Spinner size={size} className={`${styles["spinner"]}`}>
+          <img src={LoadingIcon} alt={LoadingIcon} />
+        </Spinner>
+      )}
+    </>
+  );
+};
+
+export default Loading;
