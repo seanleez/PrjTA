656ec34bf89e051b22ffac6bb8b8b2678d6271d7
Dong Lee <dong.le@sotatek.com>
2022-12-06 17:50:58 +0700

    add background

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,11 +1,18 @@
-import { Footer, Header } from './component';
+import { Footer, Header } from "./component";
+import styles from "./DefaultLayout.module.scss";
+import { Background } from "@assets/images";
 
 const DefaultLayout: React.FC<any> = ({ children }) => {
   return (
-    <div>
+    <div className={`${styles["default-layout"]}`}>
       <Header />
       <>{children}</>
       <Footer />
+      <img
+        src={Background}
+        alt={Background}
+        className={`${styles["background"]}`}
+      />
     </div>
   );
 };
