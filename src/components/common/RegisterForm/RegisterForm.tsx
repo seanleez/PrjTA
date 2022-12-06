2514b19e99ce2d417c0b731815ae8fdbad88893e
Van Vien Dong <vanviendong96@gmail.com>
2022-12-06 08:01:01 +0700

    create-register-form

---
+++ src/components/common/RegisterForm/RegisterForm.tsx
@@ -0,0 +1,82 @@
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import {
+  Button,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import styles from "./RegisterForm.module.scss";
+
+interface IRegister {
+  title: "Register" | "Edit Information";
+  onSubmit?: () => void;
+}
+
+const RegisterForm: React.FC<IRegister> = ({ title, onSubmit }) => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={onSubmit} className={`${styles["form-container"]}`}>
+      <Typography>{title}</Typography>
+      <TextField required name="name" label="User Name" />
+      <FormControl>
+        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password"
+          type={showPassword ? "text" : "password"}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={() => setShowPassword(!showPassword)}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Password"
+        />
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="outlined-adornment-confirm-password">
+          Confirm Password
+        </InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-confirm-password"
+          type={showPassword ? "text" : "password"}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={() => setShowPassword(!showPassword)}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Confirm Password"
+        />
+      </FormControl>
+      <Button className={`${styles["submit-button"]}`}>
+        {title === "Register" ? "Sign up" : "Update"}
+      </Button>
+    </form>
+  );
+};
+
+export default RegisterForm;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,12 @@
+import { RegisterForm } from "@components/common/RegisterForm";
+
 const Home: React.FC = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <h1>Home Page</h1>
+      <RegisterForm title="Register" />
+    </>
+  );
 };
 
 export default Home;
